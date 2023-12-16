9a0833d5c32f53fb5a25209bcdc3f52527472805
jltorres29 <jltorres29@hotmail.com>
2023-12-16 14:45:17 +0100

    afinando detalles

--- controllers/riders.controller.js
+++ controllers/riders.controller.js
@@ -1,49 +1,51 @@
-const Rider = require('../models/Rider.model');
+const Rider = require("../models/Rider.model");
 
-module.exports.list = function(req, res, next) {
-  Rider.find()
-    .then(riders => res.render("riders/list", { riders }))
-    .catch(error => next(error));
-}
+module.exports.list = function (req, res, next) {
+  const query = {};
 
-module.exports.details = function(req, res, next) {
-  const {id} = req.params
+  if (req.query.search) {
+    const regex = new RegExp(req.query.search, 'i');
+
+    query.name ={ $regex: regex };
+  }
+
+  Rider.find(query)
+    .then((riders) => res.render("riders/list", { riders }))
+    .catch((error) => next(error));
+};
+
+module.exports.details = function (req, res, next) {
+  const { id } = req.params;
   Rider.findById(id)
 
-    .then(rider => res.render("riders/details", { rider }))// meter un condicional de errores
-    .catch(error => next(error));
-}
-
+    .then((rider) => res.render("riders/details", { rider })) // meter un condicional de errores
+    .catch((error) => next(error));
+};
 
 //aqui get
-module.exports.create = (req, res, next) =>{
-  res.render("riders/form")// meter un condicional de errores
-  
-}
+module.exports.create = (req, res, next) => {
+  res.render("riders/form"); // meter un condicional de errores
+};
 
 //aqui post
-module.exports.doCreate = function(req, res, next) {
- 
-  Rider.create(req.body)
-    then(riderDB => res.redirect(`/riders/${id}`))
-    .catch(err => {
-      //Comprobar err instanceof mongoose.ValidationError
+module.exports.doCreate = function (req, res, next) {
+  Rider.create(req.body);
+  then((riderDB) => res.redirect(`/riders/${id}`)).catch((err) => {
+    //Comprobar err instanceof mongoose.ValidationError
 
-      next(err)
-    })
-}
+    next(err);
+  });
+};
 
-module.exports.update = (req, res, next) =>{
-  const {id} = req.params;
+module.exports.update = (req, res, next) => {
+  const { id } = req.params;
   Rider.findById(id)
-  .then(rider => res.render("riders/form", { rider }))// meter un condicional de errores
-    .catch(error => next(error));
+    .then((rider) => res.render("riders/form", { rider })) // meter un condicional de errores
+    .catch((error) => next(error));
+};
 
-}
-
-
-module.exports.doUpdate = function(req, res, next) {
-  const {id} = req.params;
+module.exports.doUpdate = function (req, res, next) {
+  const { id } = req.params;
   const updates = { ...req.body };
 
   if (req.file) {
@@ -51,18 +53,17 @@ module.exports.doUpdate = function(req, res, next) {
   }
 
   Rider.findByIdAndUpdate(id, updates, { new: true })
-    .then(riderDB => res.redirect(`/riders/${id}`))
-    .catch(err => {
+    .then((riderDB) => res.redirect(`/riders/${id}`))
+    .catch((err) => {
       //Comprobar err instanceof mongoose.ValidationError
 
-      next(err)
-    })
-}
+      next(err);
+    });
+};
 
-module.exports.delete = (req, res, next) =>{
-  const {id} = req.params;
+module.exports.delete = (req, res, next) => {
+  const { id } = req.params;
   Rider.findByIdAndDelete(id)
-  .then(rider => res.redirect("/riders"))
-    .catch(error => next(error));
-}
-
+    .then((rider) => res.redirect("/riders"))
+    .catch((error) => next(error));
+};
